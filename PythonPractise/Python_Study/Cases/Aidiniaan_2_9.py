3a3dcd5b7a79812732b3a3844fcc6cb871fc71b5
Bagda <bagdasar.aydinyan2006@gmail.com>
2024-10-29 19:41:54 +0300

    Lol

--- PythonPractise/Python_Study/Cases/Aidiniaan_2_9.py
+++ PythonPractise/Python_Study/Cases/Aidiniaan_2_9.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 '''
 Даны x, y, z.
 Вычислить выражения U и V
@@ -15,21 +14,3 @@ U = sin(abs((y - sqrt(abs(x)))*(x - y/d)))
 
 print('Выражение V равно %4.2f' %V)
 print('Выражение U равно %4.2f' %U)
-=======
-'''
-Даны x, y, z.
-Вычислить выражения U и V
-'''
-from math import *
-
-x = float(input('Введите число x: '))
-y = float(input('Введите число y: '))
-z = float(input('Введите число z: '))
-
-d = z*z + (x*x)/4
-V = cos(d)
-U = sin(abs((y - sqrt(abs(x)))*(x - y/d)))
-
-print('Выражение V равно %4.2f' %V)
-print('Выражение U равно %4.2f' %U)
->>>>>>> Stashed changes
