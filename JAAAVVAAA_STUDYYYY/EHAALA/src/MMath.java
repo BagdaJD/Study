91ba3cd63a045e0f66021819c8f15a1316833cf8
BagdaJD <bagdasar.aydinyan2006@gmail.com>
2024-10-31 11:19:54 +0300

    NineL__MMath

---
+++ JAAAVVAAA_STUDYYYY/EHAALA/src/MMath.java
@@ -0,0 +1,31 @@
+public class MMath {
+    public static double module(double a){
+        double A = a;
+
+        if (A < 0) A = -A;
+
+        return A;
+    }
+
+    public static int module(int a){
+        int A = a;
+
+        if (A < 0) A = -A;
+
+        return A;
+    }
+
+    public static int okrug(double num) {
+        double Num = num;
+        int iNum = (int) Num;
+        double lNum = iNum;
+        double rNum = iNum + 1;
+
+        if (module(Num - lNum) > module(Num - rNum)){
+            iNum += 1;
+        }
+
+        return iNum;
+    }
+
+}
--- JAAAVVAAA_STUDYYYY/EHAALA/src/Main.java
+++ JAAAVVAAA_STUDYYYY/EHAALA/src/Main.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Box box1 = new Box(10, 12, 13);
-        Box box2 = new Box(4, 5, 6);
 
-        System.out.println(box1.srBoxes(box2));
+
+
+    public static void main(String[] args) {
+         double a = 10.4;
+         int b = MMath.okrug(a);
+         System.out.println(b);
     }
 }
