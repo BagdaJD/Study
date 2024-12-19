fc05b2d465a46f7621b40fa2685b605a80cf2d6c
Bagda <bagdasar.aydinyan2006@gmail.com>
2024-12-19 22:20:12 +0300

    Lol1

--- KotlinStudy/src/Main.kt
+++ KotlinStudy/src/Main.kt
@@ -1,13 +1,9 @@
-import kotlin.math.*
+import kotlin.math.*;
 
-fun main() {
-    // put your code here
-    var x1: Int = readln().toInt()
-    var y1: Int = readln().toInt()
-    var x2: Int = readln().toInt()
-    var y2: Int = readln().toInt()
+fun main(){
+    var a: Int = readln().toInt()
+    var b: Int = readln().toInt()
 
-    var p = abs(x1 - x2) + abs(y1 - y2)
-
-    println(p)
+    var c = sqrt(Math.pow(a.toDouble(), 2.0) + Math.pow(b.toDouble(), 2.0))
+    println(c)
 }
