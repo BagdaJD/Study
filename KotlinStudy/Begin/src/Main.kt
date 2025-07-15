45a503ca862008ae1299e6cb7a58f01dac83ef03
BagdasarJD <bagdasar.aydinyan2006@gmail.com>
2025-07-15 14:45:28 +0300

    SomeKotlin

--- KotlinStudy/Begin/src/Main.kt
+++ KotlinStudy/Begin/src/Main.kt
@@ -1,34 +1,27 @@
-<<<<<<< Updated upstream:KotlinStudy/src/Main.kt
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import kotlin.math.*
-=======
-=======
-package Kot
-
->>>>>>> Stashed changes:KotlinStudy/src/Kot/Main.kt
-import kotlin.math.*;
->>>>>>> origin/main
-
+import kotlin.math.tanh
+var name: String? = "Str"
 fun main(){
-    var a: Int = readln().toInt()
-    var b: Int = readln().toInt()
-
-<<<<<<< HEAD
-    var p = abs(x1 - x2) + abs(y1 - y2)
-
-    println(p)
+    if (name != null) {
+        if (name?.length!! > 5) {
+            println("Больше 5-ти")
+        } else {
+            println("Меньше 5-ти")
+        }
+    }
+    println(getSquareParall(10))
+    val s: String = "345"
+    val i: Int = s!!.toInt()
 }
-=======
 
-fun main(){
-    var n: String = readln()
-
-    println(n.toInt() / Math.pow(10.0, n.length.toDouble()))    
+fun getSquareParall(a: Int, b: Int = a, c: Int = a) = a * b * c
+fun printInfo(lastName: String = "", name: String = "", patronymic: String = "") {
+    if(lastName.isNotEmpty()){
+        println("Фамилия: $lastName")
+    }
+    if(name.isNotEmpty()){
+        println("Имя: $name")
+    }
+    if(patronymic.isNotEmpty()){
+        println("Отчество: $patronymic")
+    }
 }
->>>>>>> Stashed changes
-=======
-    var c = sqrt(Math.pow(a.toDouble(), 2.0) + Math.pow(b.toDouble(), 2.0))
-    println(c)
-}
->>>>>>> origin/main
---
+++ KotlinStudy/FunctionalProgramming/src/Main.kt
@@ -0,0 +1,43 @@
+import kotlin.random.Random
+
+fun main(){
+    val listOfNumbers = mutableListOf<Int>()
+    for(i in 0..999){
+        listOfNumbers.add(Random.nextInt(from=0, until=1000))
+        println(i)
+    }
+    val newListOfNumbers = listOfNumbers.filter{it % 3 == 0 || it % 5 == 0}.map{it * it}.sortedDescending().map{"$it"}
+    for(i in newListOfNumbers){
+        println(i)
+    }
+
+}
+
+
+
+/*fun main() {
+    val perim: (Int, Int) -> Int = {a, b -> 2 * (a + b)}
+    val sayHello: (String) -> Unit = {it -> println("Hello $it")}
+    //val sortArr: (Array<Int>) -> Array<Int> = {it -> it.sortedArrayDescending()}
+
+    val sortArr: (Array<Int>) -> Array<Int> = {
+        for(i in it.size - 2 downTo 0){
+            for(j in 0..i){
+                if(it[j] < it[j + 1]){
+                    var t = it[j]
+                    it[j] = it[j + 1]
+                    it[j + 1] = t
+                }
+            }
+        }
+        it
+    }
+
+    println(perim(10, 12))
+    println(sayHello("Baga"))
+    val arr = sortArr(arrayOf(1, 2, 3, 4, 5))
+
+    for(i in arr){
+        println(i)
+    }
+}*/
