ede59405838c677a9552f1949130d9f941e47d81
BagdasarJD <bagdasar.aydinyan2006@gmail.com>
2025-07-22 21:46:20 +0300

    ZIP func in Kotlin

--- KotlinStudy/FunctionalProgramming/src/Main.kt
+++ KotlinStudy/FunctionalProgramming/src/Main.kt
@@ -1,14 +1,77 @@
-fun main(){
-    val array = generateSequence(0) {it + 2}
-    val evenList = array.take(10)
+fun generatePhoneNumbers(size: Int) : MutableList<String>{
+    val listOfNumbers: MutableList<String> = mutableListOf()
+    var firstNum = (Math.random()*999).toInt()
+    var secondNum = (Math.random()*999).toInt()
+    var thirdNum = (Math.random()*99).toInt()
+    var fourthNum = (Math.random()*99).toInt()
+    var strNum = "+7-$firstNum-$secondNum-$thirdNum-$fourthNum"
+    for(i in 1..size){
+        listOfNumbers.add(strNum)
+        firstNum = (Math.random()*999).toInt()
+        secondNum = (Math.random()*999).toInt()
+        thirdNum = (Math.random()*99).toInt()
+        fourthNum = (Math.random()*99).toInt()
+        strNum = "+7-$firstNum-$secondNum-$thirdNum-$fourthNum"
+    }
+    return listOfNumbers
+}
 
-    for(i in  evenList){
-        println(i)
+fun generateNames(size: Int) : MutableList<String>{
+    val listOfNames: MutableList<String> = mutableListOf()
+    for(i in 1..1000){
+        listOfNames.add("Name$i")
+    }
+
+    return listOfNames
+}
+
+fun main(){
+    val listOfNamesLastNames: MutableList<String> = mutableListOf()
+    for(i in 0..<1000){
+        listOfNamesLastNames.add("Name$i LastName$i")
+    }
+
+    //val names = listOfNamesLastNames.map{it.substring(0, 8)}
+    val names = listOfNamesLastNames.map{it.substringAfter(" ")}
+    val lastNames = listOfNamesLastNames.map{it.substring(7)}
+    val listOfPairs = names.zip(lastNames)
+    for(f in listOfPairs){
+        println(f)
     }
 }
 
 
 /*
+    val listOfNumbers = generatePhoneNumbers(1000)
+    val listOfNames = generateNames(1000)
+    for(i: Int in 0..<listOfNames.size){
+        println(listOfNames[i])
+        println(listOfNumbers[i])
+    }
+    val fullNames = listOfNames.zip(listOfNumbers)
+    for(user in fullNames){
+        println(user.first + user.second)
+    }
+*/
+/*
+fun main(){
+
+    val array = generateSequence("Worker №1"){
+        val index = it.substring(8).toInt()
+        "Worker №${index + 1}"
+
+    }
+
+
+    val resList = array.take(100)
+    for(i: String in resList){
+        println(i)
+    }
+
+}
+
+
+
 import kotlin.random.Random
 fun main(){
     val listOfNumbers = mutableListOf<Int>()
