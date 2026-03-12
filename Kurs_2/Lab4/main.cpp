d1d99a40c5d4edb75a175dc07756fe75cbb3745a
Bagdasar Aydinyan <bagdasar.aydinyan2006@gmail.com>
2026-03-12 13:25:18 +0300

    Lolol

--- Kurs_2/Lab4/main.cpp
+++ Kurs_2/Lab4/main.cpp
@@ -1,25 +1,29 @@
 #include "Circle.h"
+#include<fstream>
 
 int main()
 {
-    Circle circle;
-    cout << circle.getR() << endl;
-    circle.setR(-5);
-    circle.setType("Lolo");
-    cout << circle.getType() << endl;
+    ifstream read("text.txt");
 
-    circle.showInfo();
-    cout << "---------------" << endl;
 
-    Cone cone;
-    cone.showInfo();
-
-    Sphere sphere;
-    sphere.showInfo();
-
-    cout << "---------------" << endl;
-    Cylinder c1;
-    c1.showInfo();
+    // Circle circle;
+    // cout << circle.getR() << endl;
+    // circle.setR(-5);
+    // circle.setType("Lolo");
+    // cout << circle.getType() << endl;
+    //
+    // circle.showInfo();
+    // cout << "---------------" << endl;
+    //
+    // Cone cone;
+    // cone.showInfo();
+    //
+    // Sphere sphere;
+    // sphere.showInfo();
+    //
+    // cout << "---------------" << endl;
+    // Cylinder c1;
+    // c1.showInfo();
 
     return 0;
 }
